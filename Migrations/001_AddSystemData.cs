4b22c6a37ef697f2421545c9dc53a766715717da
benwiley <ben.wiley@newspring.cc>
2015-03-16 11:15:33 -0400

    Fix spelling

--- Migrations/001_AddSystemData.cs
+++ Migrations/001_AddSystemData.cs
@@ -38,7 +38,7 @@ namespace cc.newspring.AttendedCheckIn.Migrations
             RockMigrationHelper.AddLayout( "30FB46F7-4814-4691-852A-04FB56CC07F0", "Checkin", "Checkin", "", "3BD6CFC1-0BF2-43C8-AD38-44E711D6ACE0" );
 
             Sql( @"
-                UPDATE [Site] SET [IsSytem] = 0 WHERE [Guid] = '30FB46F7-4814-4691-852A-04FB56CC07F0'
+                UPDATE [Site] SET [IsSystem] = 0 WHERE [Guid] = '30FB46F7-4814-4691-852A-04FB56CC07F0'
             " );
 
             // Attended Check-in root page (no blocks)
