53945b8bdcddd642b015e931c42a4186edd32349
dcs619 <dcs619@gmail.com>
2015-03-25 16:47:23 -0400

    Use minified CSS in themes

--- scripts.js
+++ scripts.js
@@ -2,7 +2,7 @@
     var _previousDOB = '';
 
     var loadStyles = function () {
-        var relPath = '../plugins/cc_newspring/attendedcheckin/Styles/styles.css';
+        var relPath = '../plugins/cc_newspring/attendedcheckin/Styles/styles.min.css';
         var styleLink = $('<link>').attr('rel', 'stylesheet').attr('href', relPath);
         $('head').append(styleLink);
     };
