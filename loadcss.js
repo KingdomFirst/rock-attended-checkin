3433309871435adeed03596381d62efce5173131
dcs619 <dcs619@gmail.com>
2015-08-24 10:55:43 -0400

    recompile checkin themes, confirmed bootstrap button still broken

--- loadcss.js
+++ loadcss.js
@@ -1,5 +1,5 @@
 ﻿var loadCSS = function () {
-    var relPath = '../plugins/cc_newspring/attendedcheckin/Styles/styles.min.css';
+    var relPath = '../plugins/cc_newspring/attendedcheckin/Styles/styles.css';
     var styleLink = $('<link>').attr('rel', 'stylesheet').attr('href', relPath);
     $('head').append(styleLink);
 }();
