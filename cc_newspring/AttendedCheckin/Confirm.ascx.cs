2b9bbfaf3d47632a3dff4dafc421447b5d15291e
dcs619 <dcs619@gmail.com>
2016-02-10 11:50:55 -0500

    Updated description

--- cc_newspring/AttendedCheckin/Confirm.ascx.cs
+++ cc_newspring/AttendedCheckin/Confirm.ascx.cs
@@ -46,7 +46,7 @@ namespace RockWeb.Plugins.cc_newspring.AttendedCheckin
     [LinkedPage( "Activity Select Page" )]
     [BooleanField( "Display Group Names", "By default location names are shown in the grid.  Check this option to show the group names instead.", false )]
     [BooleanField( "Print Individual Labels", "Select this option to print one label per person's group, location, & schedule.", false )]
-    [BooleanField( "Remove Attendance On Checkout", "Select this option to remove a person's attendance on checkout.", false )]
+    [BooleanField( "Remove Attendance On Checkout", "By default, the attendance is given a checkout date.  Select this option to completely remove attendance on checkout.", false )]
     [BinaryFileField( "DE0E5C50-234B-474C-940C-C571F385E65F", "Designated Single Label", "Select a label to print once per print job.  Unselect the label to print it with every print job.", false )]
     public partial class Confirm : CheckInBlock
     {
